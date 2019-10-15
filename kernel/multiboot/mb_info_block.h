e57b65df92b01442d662e7cab3c68f8ff952fc3d
DorianXGH <dorian.bourgeoisat@icloud.com>
2019-10-15 23:05:19 +0200

    Fixed multiboot info struct

--- kernel/multiboot/mb_info_block.h
+++ kernel/multiboot/mb_info_block.h
@@ -13,15 +13,17 @@ struct mb_info_block {
     uint32_t sym[4];
     uint32_t mmap_length;
     uint32_t mmap_addr;
+    uint32_t drives_length
+    uint32_t drives_addr;
     uint32_t config_table;
     uint32_t boot_loader_name;
     uint32_t apm_table;
     uint32_t vbe_control_info;
     uint32_t vbe_mode_info;
-    uint32_t vbe_mode;
-    uint32_t vbe_interface_seg;
-    uint32_t vbe_interface_off;
-    uint32_t vbe_interface_len;
+    uint16_t vbe_mode;
+    uint16_t vbe_interface_seg;
+    uint16_t vbe_interface_off;
+    uint16_t vbe_interface_len;
     uint64_t framebuffer_addr;
     uint32_t framebuffer_pitch;
     uint32_t framebuffer_width;
