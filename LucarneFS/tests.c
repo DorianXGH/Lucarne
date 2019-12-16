8094d54eb6f27243956d8632447f525da3d9d01e
DorianXGH <dorian.bourgeoisat@icloud.com>
2019-12-16 21:22:39 +0100

    added moar test

--- LucarneFS/tests.c
+++ LucarneFS/tests.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 int main(int argc, char * argv[])
 {
+    printf("%d\n", sizeof(struct LuFS_volume_identifier));
+    printf("%d\n", sizeof(struct LuFS_block_pointer));
     printf("%d\n", sizeof(struct LuFS_master_block));
     return 0;
 }
